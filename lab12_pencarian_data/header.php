03511c8444e10978734894688fc7a98ef64777d1
unknown <defapabela@gmail.com>
2022-12-25 02:58:01 +0700

    menambahkan file

---
+++ lab12_pencarian_data/header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Login</title>
+    <link href="style.css" rel="stylesheet" type="text/stylesheet" media="screen" />
+</head>
+
+<body>
+    <div class="container">
+        <header>
+            <h1></h1>
+        </header>
+       
---
+++ lab12_pencarian_data/index.php
@@ -0,0 +1,72 @@
+<?php require('header.php'); ?>
+<?php
+include("koneksi.php");
+
+// query untuk menampilkan data
+$sql = 'SELECT * FROM data_barang';
+$result = mysqli_query($conn, $sql);
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <link href="style.css" rel="stylesheet" type="text/css" />
+    <title>Data Barang</title>
+</head>
+
+<body>
+    <div class="container">
+
+        <h2>Data Barang</h2>
+        <nav>
+<a href="home.php">Home</a>
+<a href="about.php">Tentang</a>
+<a href="kontak.php">Kontak</a>
+</nav>
+
+        <a href="tambah.php">Tambah Barang</a>
+        <br></br>
+        
+
+        <div class="main">
+            <table border="1" cellpadding="10" cellspacing="0">
+                <tr>
+                    <th>Gambar</th>
+                    <th>Nama Barang</th>
+                    <th>Katagori</th>
+                    <th>Harga Jual</th>
+                    <th>Harga Beli</th>
+                    <th>Stok</th>
+                    <th>Aksi</th>
+                </tr>
+                <?php if ($result) : ?>
+                    <?php while ($row = mysqli_fetch_array($result)) : ?>
+                        <tr>
+                            <td><img src="gambar/<?= $row['gambar']; ?>" alt="<?= $row['nama']; ?>"></td>
+                            <td><?= $row['nama']; ?></td>
+                            <td><?= $row['kategori']; ?></td>
+                            <td><?= $row['harga_beli']; ?></td>
+                            <td><?= $row['harga_jual']; ?></td>
+                            <td><?= $row['stok']; ?></td>
+                            <td>
+                                <a href="ubah.php?id=<?= $row['id_barang']; ?>">ubah</a> |
+                                <a href="hapus.php?id=<?= $row['id_barang']; ?>" onclick="
+                                    return confirm('apakah anda yakin ingin menghapus?');">hapus</a>
+                            </td>
+                        </tr>
+                    <?php endwhile;
+                else : ?>
+                    <tr>
+                        <td colspan="7">Belum ada data</td>
+                    </tr>
+                <?php endif; ?>
+            </table>
+        </div>
+    </div>
+    <br><br />
+
+    <?php require('footer.php'); ?>
+</body>
+
+</html>
